1595fb94056f8b8817f037201d1134572102be26
Kevin Shen <kevshin222@Hotmail.com>
2012-11-15 09:49:27 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,38 +12,37 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		String inputFileName, outputFileName;
-        inputFileName = outputFileName = null;
+        	inputFileName = outputFileName = null;
 		if (args.length > 2) {
-            System.err.println("Error: Invalid input, 1 input"
-                + " file and 1 output file.");
-            System.exit(1);
-        } else if (args.length == 0) {
-            usage();
-        } else if (args.length == 2) {
-        	inputFileName = args[0];
-            outputFileName = args[1];
-        } else {
-        	System.err.println("Error: Missing input or output file.");
-            System.exit(1);
-        }
+            		System.err.println("Error: Invalid input, 1 input"
+               		+ " file and 1 output file.");
+            		System.exit(1);
+        	} else if (args.length == 0) {
+            		usage();
+        	} else if (args.length == 2) {
+        		inputFileName = args[0];
+           		 outputFileName = args[1];
+        	} else {
+        		System.err.println("Error: Missing input or output file.");
+            		System.exit(1);
+        	}
 		if (inputFileName != null) {
-            try {
-                System.setIn(new FileInputStream(inputFileName));
-            } catch (FileNotFoundException e) {
-                System.err.printf("Error: could not open %s%n", inputFileName);
-                System.exit(1);
-            }
-        }
+            		try {
+                		System.setIn(new FileInputStream(inputFileName));
+            		} catch (FileNotFoundException e) {
+                		System.err.printf("Error: could not open %s%n", inputFileName);
+               			System.exit(1);
+            		}
+        	}
 		if (outputFileName != null) {
-            try {
-                System.setOut(new PrintStream(outputFileName));
-            } catch (FileNotFoundException e) {
-                System.err.printf("Error: could not open %s%n",
-                                  outputFileName);
-                System.exit(1);
-            }
-        }
-		
+            		try {
+                		System.setOut(new PrintStream(outputFileName));
+            		} catch (FileNotFoundException e) {
+                		System.err.printf("Error: could not open %s%n",
+                                  	outputFileName);
+                		System.exit(1);
+            		}
+       		}
 		Scanner scan = new Scanner(System.in);
 		while (scan.hasNextLine()) {
 			String next = scan.nextLine();
@@ -66,16 +65,16 @@ public class Main {
 			slash = slash.substring(0, slash.length() - 1);
 			System.out.println(slash);
 		}
-        System.out.close();
+        	System.out.close();
 	}
 	
 	/** Print brief description of the command-line format. */
-    static void usage() {
-    	String[] usage = new String[] {"Usage:", "1. Compile the program using javac Main.java",
-    		"2. Run the program using java Main INPUTFILENAME OUTPUTFILENAME"};
-        for (String i:usage) {
-        	System.out.println(i);
-        }
-        System.exit(1);
-    }
+    	static void usage() {
+    		String[] usage = new String[] {"Usage:", "1. Compile the program using javac Main.java",
+    			"2. Run the program using java Main INPUTFILENAME OUTPUTFILENAME"};
+        	for (String i:usage) {
+        		System.out.println(i);
+        	}
+        	System.exit(1);
+	}
 }
